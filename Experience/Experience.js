b01e6fcbea4fd510446c0f2735d2e8ff79cc4a7d
Ben <bteg0001@student.monash.edu>
2023-02-13 16:32:01 +1100

    Basic Singleton Structure for Three.js completed.

---
+++ Experience/Experience.js
@@ -0,0 +1,51 @@
+import * as THREE from "three";
+
+import Sizes from "./Utils/Sizes.js"; 
+import Time from "./Utils/Time.js"; 
+
+import Camera from "./Camera.js";
+import Renderer from "./Renderer";
+
+import World from "./World/World.js"
+;
+export default class Experience {
+    // Static Instance Variable
+    static instance
+
+    constructor(canvas) {
+
+        // Check to see if Instance Experience exists
+        if(Experience.instance){
+            // IF Does exist, do this
+            return Experience.instance
+        }
+
+        // IF Doesn't exist, do this 
+        Experience.instance = this
+        this.canvas = canvas;
+        this.scene = new THREE.Scene();
+        this.sizes = new Sizes();
+        this.time = new Time();
+        this.camera = new Camera();
+        this.renderer = new Renderer();
+        this.world = new World();
+
+        this.time.on("update", ()=>{
+            this.update();
+        });
+
+        this.sizes.on("resize", ()=>{
+            this.resize();
+        });
+    }
+
+    resize(){
+        this.camera.resize();
+        this.renderer.resize();
+    }
+
+    update(){
+        this.camera.update();
+        this.renderer.update();
+    }
+}
